b0efc3ea00aae630bed86863715b57ebeb57dc6c
HungLEEE2709 <hungle300nam@gmail.com>
2025-06-23 10:07:13 +0700

    fix

--- Assets/Scripts/EvnController/BackGroundCotroller.cs
+++ Assets/Scripts/EvnController/BackGroundCotroller.cs
@@ -1,34 +1,37 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class BackgroundController : MonoBehaviour
 {
-    private float startPos, length;
-    public GameObject cam;
-    public float parallaxEffect; // 0 = theo camera hoàn toàn, 1 = không di chuyển, 0.5 = di chuyển nửa tốc độ
+    public Transform mainCam;
+    public Transform MidBg;
+    public Transform SideBg;
+    public float length;
 
     void Start()
     {
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        if (mainCam == null && Camera.main != null)
+            mainCam = Camera.main.transform;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        // Tính khoảng cách camera đã di chuyển (tỉ lệ parallax)
-        float distance = cam.transform.position.x * parallaxEffect;
-        float movement = cam.transform.position.x * (1 - parallaxEffect);
-
-        // Cập nhật vị trí background theo chuyển động camera
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
-
-        // Nếu background đã vượt khỏi chiều dài, thì dịch ngược lại để cuộn vô tận
-        if (movement > startPos + length)
+        if (mainCam.position.x > MidBg.position.x)
         {
-            startPos += length;
+            UpdateBackGroundPosition(Vector3.right);
         }
-        else if (movement < startPos - length)
+        else if (mainCam.position.x < MidBg.position.x)
         {
-            startPos -= length;
+            UpdateBackGroundPosition(Vector3.left);
         }
+    }
+
+    void UpdateBackGroundPosition(Vector3 direction)
+    {
+        SideBg.position = MidBg.position+direction*length;
+        Transform temp = MidBg;
+        MidBg = SideBg;
+        SideBg = temp;
     }
 }
