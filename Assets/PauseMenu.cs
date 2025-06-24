5139e94f6a78a5420a84b9d12bcebaaaf9e15c14
Tung090505 <2331540153@vaa.edu.vn>
2025-06-24 16:22:13 +0700

    add PauseGame

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject resumeMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
